00e7d8d56254a2470c84b0c759f25f7d82a3ed7d
Jon Myrick <jonmyrick7401@gmail.com>
2017-03-07 21:11:19 -0700

    adding menu

---
+++ src/components/common/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+let Menu = require('react-burger-menu').slide;
+
+class Example extends React.Component{
+  showSettings(event) {
+    event.preventDefault()
+
+  }
+  render() {
+    return (
+      <Menu style={{width: '20px', height: '20px'}}>
+        <a id="home" className="menu-item">Home</a>
+        <a id="about" className="menu-item">About</a>
+        <a id="contact" className="menu-item">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+    );
+  }
+}
+
+export default Example; 
