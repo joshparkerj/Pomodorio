8cb378d3a137288d3fb18128623063a711b30c0e
Jon Myrick <jonmyrick7401@gmail.com>
2017-03-08 15:49:17 -0700

    added new sideNav

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -3,10 +3,10 @@ import {Link, IndexLink} from 'react-router';
 
 const Header = () => {
   return (
-    <nav style={{display: 'flex', margin:'0px 20px 20px', height:'65px', width: 'calc(100vw - 230px)', fontSize: '15px', fontWeight: '300', borderBottom: '1px solid grey', borderRadius: 'initial'}} className='navbar'>
+    <section style={{display: 'flex', margin:'0px 20px 20px', height:'65px', width: 'calc(100vw - 230px)', fontSize: '15px', fontWeight: '300', borderBottom: '1px solid grey', borderRadius: 'initial'}} className='myNavbar'>
       <div className='hover'><IndexLink to="/" style={{textDecoration: 'none', margin: '5px'}}>Pomodoro</IndexLink></div>
       <div className='hover'><Link to="/settings" style={{textDecoration: 'none', margin: '5px'}}>Settings</Link></div>
-    </nav>
+    </section>
   );
 };
 
---
+++ src/components/common/SideNav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {SideNav, SideNavItem, Button} from 'react-materialize';
+import {Link, IndexLink} from 'react-router';
+import HamIcon from './Icons/HamIcon';
+export default () => {
+  return (
+   <SideNav
+      trigger={<div><HamIcon /></div>}
+      options={{ closeOnClick: true }}
+      >
+      <SideNavItem userView
+        user={{
+          background: 'http://www.footballreplay.net/wp-content/uploads/2016/06/metal-grill-background-1024x1024-300x300.jpg',
+          image: 'http://www.iconskid.com/images/56/andrew-avatar-head-human-man-user-icon-search-engine-56456.png',
+          name: 'Jon Myrick',
+          email: 'jonmyrick@gmail.com'
+        }}
+      />
+     
+      <SideNavItem >Pomodoro</SideNavItem>
+      <SideNavItem >Settings</SideNavItem>
+      <SideNavItem divider />
+      
+      <SideNavItem waves>Logout</SideNavItem>
+    </SideNav>
+  );
+};
+
