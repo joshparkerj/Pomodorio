8d3d44da986e581193a9ed50ff66a7576236ad1e
Jon Myrick <jonmyrick7401@gmail.com>
2017-03-09 12:28:14 -0700

    added svg to timer component. Clock now displays current time

--- src/components/PomodoroPage/Timer.js
+++ src/components/PomodoroPage/Timer.js
@@ -40,11 +40,9 @@ class Timer extends React.Component{
     let blink = this.state.blink;
     return(
       <div  className='col-xs-12 col-sm-6 col-md-6'>
-        <div className='components timer'>
-          {/*<svg className ="digitalClock" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 58">
-            <g id="Layer1" data-name="Layer 2">
-              <rect className="cls-1" x="0.5" y="0.5" width="94.95" height="56.94" rx="12" ry="12"/>
-              <rect className="cls-2" x="14.66" y="17.47" width="68.74" height="24.19"/>
+        <div className='components timer' style={{display:'flex', justifyContent: 'center'}}>
+          <svg className ="digitalClock" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 42" style={{height: '100%'}}>
+              <g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M83.5,40.93h-71c-6.6,0-12-3.83-12-8.51V9C.5,4.33,5.9.5,12.5.5H83.4c6.6,0,12,3.83,12,8.51V32.35C95.5,37.1,90.1,40.93,83.5,40.93Z"/></g>
               <foreignObject width="100%" height="100%">
                   <div xmlns="http://www.w3.org/1999/xhtml" className="f-object">
                     <div style={styles.numbers}>
@@ -52,19 +50,24 @@ class Timer extends React.Component{
                       </div>
                   </div>
               </foreignObject>
-            </g>
-          </svg>*/}
+           
+          </svg>
         </div>
       </div>
     );
   }
 }
-//<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 95.9 41.43"><defs><style>.cls-1{fill:#121212;stroke:#242424;stroke-miterlimit:10;}</style></defs><title>timer</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M83.5,40.93h-71c-6.6,0-12-3.83-12-8.51V9C.5,4.33,5.9.5,12.5.5H83.4c6.6,0,12,3.83,12,8.51V32.35C95.5,37.1,90.1,40.93,83.5,40.93Z"/></g></g></svg>
+
+
 export default Timer;
 
 let styles = {
   numbers: {
-    fontSize: 20,
+    padding: '10px',
+    display:'flex', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 25,
     fontFamily: 'digital-clock-font',
     letterSpacing: '2px',
     color:'rgb(51,	213,	229)'
